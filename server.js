1782b3e9c61ecb6d1d688386bb910ae96510e2f6
LouiseFuglsang <louise.mortensen@gmail.com>
2023-01-02 10:42:46 +0100

    Added POST to database

--- server.js
+++ server.js
@@ -1,20 +1,90 @@
+//npm init (danner package.json)
+//npm i express mongoose (danner package-lock.json)
+//cd til rette mappe
+//node server.js (eller nodemon server.js)
+
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const ejs =require('ejs');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
 
+
+mongoose.set('strictQuery', false);
+
+
+// ========================
+// Middlewares
+// ========================
+
+// Static files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Set views
 app.set('view engine', 'ejs');
 
-mongoose.connect('mongodb+srv://DinStoreFest:3NzTWdILKT3E6bHk@cluster0.2nzwv.mongodb.net/?retryWrites=true&w=majority');
 
+// ========================
+// Connection to mongoDB
+// ========================
+
+//Kodeordet skal sikres!!!
+mongoose.connect('mongodb+srv://Louise:GoPikachu78@cluster0.k9wdj.mongodb.net/messagesDB?retryWrites=true&w=majority', {useNewUrlParser: true}, {useUnifiedTopology: true});
+
+
+// ========================
+// Data schema
+// ========================
+
+const messagesSchema = {
+    message: String,
+    from: String,
+    to: String
+}
+// ========================
+// Data model
+// ========================
+
+const Besked = mongoose.model('message', messagesSchema);
+
+
+/*
+//READ
 app.get('/', (req, res) => {
-    let name = 'Marina';
+    Besked.find({}, function(err, messages) {
+        res.render('index', {
+            messagesList: messages
+        })
+    })
+})
+*/
 
-    res.render('index', {
-            userName : name
+
+// ================================
+// Routes/Read route / handlers
+// ================================
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+})
+
+
+//app.post
+app.post('/', function(req, res){
+    let newBesked = new Besked({
+        from: req.body.from,
+        message: req.body.message
     });
-    })
+    newBesked.save();
+    res.redirect('/');
+})
 
-app.listen(3008, function() {
-    console.log('server is running');
-    })
+// ========================
+// Listen
+// ========================
+
+app.listen(4000, function(){
+    console.log("server is running on 4000");
+})
